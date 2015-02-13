aa31a30ce69c77f4795b903a19c3f89612cd7760
Crypt0s <Bryanhalf@gmail.com>
2015-02-13 13:23:53 -0500

    AAAA Records working, PTR in-progress

--- fakedns.py
+++ fakedns.py
@@ -35,6 +35,63 @@ TYPE = {
     "\x00\x10":"TXT"
 }
 
+# Stolen: https://github.com/learningequality/ka-lite/blob/master/python-packages/django/utils/ipv6.py#L209
+def _is_shorthand_ip(ip_str):
+    """Determine if the address is shortened.
+    Args:
+        ip_str: A string, the IPv6 address.
+    Returns:
+        A boolean, True if the address is shortened.
+    """
+    if ip_str.count('::') == 1:
+        return True
+    if any(len(x) < 4 for x in ip_str.split(':')):
+        return True
+    return False
+
+# Stolen: https://github.com/learningequality/ka-lite/blob/master/python-packages/django/utils/ipv6.py#L209
+def _explode_shorthand_ip_string(ip_str):
+    """
+    Expand a shortened IPv6 address.
+    Args:
+        ip_str: A string, the IPv6 address.
+    Returns:
+        A string, the expanded IPv6 address.
+    """
+    if not _is_shorthand_ip(ip_str):
+        # We've already got a longhand ip_str.
+        return ip_str
+
+    new_ip = []
+    hextet = ip_str.split('::')
+
+    # If there is a ::, we need to expand it with zeroes
+    # to get to 8 hextets - unless there is a dot in the last hextet,
+    # meaning we're doing v4-mapping
+    if '.' in ip_str.split(':')[-1]:
+        fill_to = 7
+    else:
+        fill_to = 8
+
+    if len(hextet) > 1:
+        sep = len(hextet[0].split(':')) + len(hextet[1].split(':'))
+        new_ip = hextet[0].split(':')
+
+        for _ in xrange(fill_to - sep):
+            new_ip.append('0000')
+        new_ip += hextet[1].split(':')
+
+    else:
+        new_ip = ip_str.split(':')
+
+    # Now need to make sure every hextet is 4 lower case characters.
+    # If a hextet is < 4 characters, we've got missing leading 0's.
+    ret_ip = []
+    for hextet in new_ip:
+        ret_ip.append(('0' * (4 - len(hextet)) + hextet).lower())
+    return ':'.join(ret_ip)
+
+
 class DNSResponse(object):
     def __init__(self,query):
         self.id = query.data[:2]        # Use the ID from the request.
@@ -75,9 +132,11 @@ class A(DNSResponse):
 
 # Not implemented, need to get ipv6 to translate correctly into hex
 class AAAA(DNSResponse):
-    def __init__(self,query):
+    def __init__(self,query,address):
         super(AAAA,self).__init__(query)
         self.type = "\x00\x1c"
+        self.length = "\x00\x10"
+        self.data = address # Address is already encoded properly for the response at rule-builder
 
     # Thanks, stackexchange! http://stackoverflow.com/questions/16276913/reliably-get-ipv6-address-in-python
     def get_ip_6(host, port=0):
@@ -85,7 +144,6 @@ class AAAA(DNSResponse):
         result = socket.getaddrinfo(host, port, socket.AF_INET6)
         # Will need something that looks like this: 
         ip = result[0][4][0] # just returns the first answer and only the address
-        #return map(lambda x: chr(int(x)), ip.split(':'))
 
 # Not yet implemented
 class CNAME(DNSResponse):
@@ -95,8 +153,18 @@ class CNAME(DNSResponse):
 
 # Not yet implemented
 class PTR(DNSResponse):
-    def __init__(self,query):
+    def __init__(self,query,ptr_entry):
         super(PTR,self).__init__(query)
+        self.type = "\x00\x0c"
+
+        ptr_split = ptr_entry.split('.')
+        ptr_entry = "\x07".join(ptr_split)
+
+        self.data = "\x0e" + ptr_entry + "\x00"
+        self.length = chr(len(ptr_entry)+2)
+        # Again, must be 2-byte value.
+        if self.length < '\xff':
+            self.length = "\x00"+self.length
 
 # Finished
 class TXT(DNSResponse):
@@ -104,7 +172,6 @@ class TXT(DNSResponse):
         super(TXT,self).__init__(query)
         self.type = "\x00\x10"
         self.data = txt_record
-
         self.length = chr(len(txt_record)+1)
         # Must be two bytes.
         if self.length < '\xff':
@@ -147,6 +214,15 @@ class ruleEngine:
                     print "Malformed rule : " + rule + " Not Processed."
                     continue
 
+                # We need to do some housekeeping for ipv6 rules and turn them into full addresses if they are shorts.
+                # I could do this at match-time, but i like speed, so I've decided to keep this in the rule parser and then work on the logging separate    
+                if splitrule[0] == "AAAA":
+                    if _is_shorthand_ip(splitrule[2]):
+                        splitrule[2] = _explode_shorthand_ip_string(splitrule[2])
+                    # OK Now we need to get the ip broken into something that the DNS response can have in it
+                    splitrule[2] = splitrule[2].replace(":","").decode('hex')
+                    # That is what goes into the DNS request.
+
                 # If the ip is 'self' transform it to local ip.
                 if splitrule[2] == 'self':
                     try:
@@ -157,7 +233,13 @@ class ruleEngine:
                     splitrule[2] = ip
 
                 self.re_list.append([splitrule[0],re.compile(splitrule[1]),splitrule[2]])
-                print '>>', splitrule[1], '->', splitrule[2]
+                
+                # TODO: More robust logging system - printing ipv6 rules requires specialness since I encode the ipv6 addr in-rule
+                if splitrule[0] == "AAAA":
+                    print '>>', splitrule[1], '->', splitrule[2].encode('hex')
+                else:
+                    print '>>', splitrule[1], '->', splitrule[2]
+
             print '>>', str(len(rules)) + " rules parsed"
 
     # Matching has now been moved into the ruleEngine so that we don't repeat ourselves
