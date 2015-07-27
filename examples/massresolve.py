6de2adae1bfa55fed4ee70cc9e4bac26f3fda4d9
Crypt0s <crypt0s@Crypt0s.local>
2015-07-26 20:04:27 -0400

    Add an example for using FakeDNS/DNSQuery.py for doing mass parallel lookups of DNS names.

---
+++ examples/massresolve.py
@@ -0,0 +1,23 @@
+# This is a for-fun implementation of DNSQuery.  Hurrah, this is useful!
+import socket
+import multiprocessing
+from multiprocessing import Pool
+from DNSQuery import DNSQuery
+
+def _resolve(packet):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sent = s.sendto(packet,('8.8.8.8', 53))
+    response = s.recv(1024) # probably not enough for all situations - TODO: improve this
+    s.close()
+    # I just realized I need to be able to feed a response into something in order to get the data back out...
+    return response
+
+def mass_resolve(list):
+    queries = []
+    for domain_name in list:
+        queries.append(DNSQuery(query=domain_name).packet)
+
+    query_sender_pool = Pool(4)
+    # Note: This will *not* return until all the map operations have completed _successfully_
+    results = query_sender_pool.map(_resolve,queries)
+    return results
