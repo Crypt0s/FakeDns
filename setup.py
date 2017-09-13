7f1537fa20a53f0b844efda5404a8c3ac3f48625
Crypt0s <crypt0s@Crypt0s.local>
2017-09-12 23:02:11 -0400

    Add setup file for installing FakeDNS library

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dns',
+      version='1.0',
+      description='A pure-Python implementation of the DNS protocol',
+      author='Bryan "Crypt0s" Halfpap',
+      author_email='bryanhalf@gmail.com',
+      url='https://github.com/crypt0s/FakeDns/',
+      packages=['dns'],
+     )
