a4a6bb2485b9940cd66119b86cdadf2ea591e7ba
Crypt0s <crypt0s@Crypt0s.local>
2017-09-14 22:17:36 -0400

    Various fixes to names

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='dns',
+setup(name='FakeDns',
       version='1.0',
       description='A pure-Python implementation of the DNS protocol',
       author='Bryan "Crypt0s" Halfpap',
       author_email='bryanhalf@gmail.com',
       url='https://github.com/crypt0s/FakeDns/',
-      packages=['dns'],
+      packages=['FakeDns'],
      )
