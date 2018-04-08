2989cddd2c058d3ad2a7b1e77f4e213492fc9902
Crypt0s <bryanhalf@gmail.com>
2018-04-08 16:57:44 -0400

    move tests up one

---
+++ tests/run_tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+import os
+
+sys.path.append("../../")
+import fakedns
+from fakedns import massresolve
+
+import pdb
+pdb.set_trace()
+
+if __name__ == "__main__":
+    print "Success"
+    exit(0)
